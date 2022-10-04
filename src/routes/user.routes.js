e172c320138f0d1a47ab5d02f2f3f127cdc25f46
murilo muller <murilomuller@protonmail.com>
2022-10-04 00:53:44 -0300

    userAvatarController

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,14 +1,17 @@
 const { Router } = require('express')
 const multer = require('multer')
 
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
+const UserAvatarController = require('../controllers/UserAvatarController')
+
 const UsersController = require('../controllers/UsersController')
 const uploadConfig = require('../configs/upload')
-const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
 
 const userRoutes = Router()
 const upload = multer(uploadConfig.MULTER)
 
 const usersController = new UsersController()
+const userAvatarController = new UserAvatarController()
 
 userRoutes.post('/', usersController.create)
 userRoutes.put('/:id', usersController.update)
